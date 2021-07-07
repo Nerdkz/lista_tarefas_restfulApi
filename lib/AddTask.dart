1d237cbcabd3558aafb64769e26d317d0c0cbc45
Nerdkz <viniciusmendonca@gec.inatel.br>
2021-07-07 18:56:40 -0300

    First Commit

---
+++ lib/AddTask.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AddTask extends StatefulWidget {
+
+  @override
+  _AddTaskState createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      body: ,
+    );
+  }
+}
+
